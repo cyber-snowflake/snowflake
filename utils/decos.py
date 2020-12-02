e3aafecb197f52a23b77b225b93bbb9e49418365
defracted <72914371+defracted@users.noreply.github.com>
2020-12-02 21:24:15 +0400

    Added little debugging message to aiofy decorator

--- utils/decos.py
+++ utils/decos.py
@@ -2,6 +2,8 @@ import asyncio
 from functools import wraps
 from typing import Any, Coroutine, TypeVar
 
+from loguru import logger
+
 T = TypeVar("T")
 
 
@@ -9,6 +11,7 @@ def aioify(func):
     @wraps(func)
     def wrapper(*args, **kwargs) -> Coroutine[Any, Any, T]:
         loop = asyncio.get_running_loop()
+        logger.debug(f"Running {func.__name__}")
         return loop.run_in_executor(None, lambda: func(*args, **kwargs))
 
     return wrapper
