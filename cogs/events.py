4dd123d4e4b68f0c97778ea1d7dc9081fd5419db
defracted <72914371+defracted@users.noreply.github.com>
2020-12-19 21:46:53 +0100

    Added a little more logging

--- cogs/events.py
+++ cogs/events.py
@@ -1,5 +1,6 @@
 from discord import Guild
 from discord.ext import commands
+from loguru import logger
 
 from bot import BigMommy
 
@@ -11,6 +12,13 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_guild_join(self, guild: Guild):
         await self.bot.pg.add_guild(guild.id)
+        logger.info(
+            f"Joined a {guild.name} ({guild.id}) with {guild.member_count} members, owned by {guild.owner} ({guild.owner_id})"  # noqa
+        )
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild: Guild):
+        logger.info(f"Left a guild {guild.name} ({guild.id})")
 
 
 def setup(bot):
