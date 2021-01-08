df57342c9e4af73f8ab66ab44492a634ef4a6f66
defracted <72914371+defracted@users.noreply.github.com>
2021-01-08 19:20:09 +0400

    Added activity changer task

--- cogs/events.py
+++ cogs/events.py
@@ -1,5 +1,7 @@
-from discord import Guild
-from discord.ext import commands
+import itertools
+
+from discord import Activity, ActivityType, Guild
+from discord.ext import commands, tasks
 from loguru import logger
 
 from bot import BigMommy
@@ -8,6 +10,21 @@ from bot import BigMommy
 class Events(commands.Cog):
     def __init__(self, bot: BigMommy):
         self.bot = bot
+        self.activities = itertools.cycle(
+            [
+                Activity(name=f"over {len(self.bot.users)} users", type=ActivityType.watching),
+                Activity(name=f"with {len(self.bot.guilds)} guilds", type=ActivityType.playing),
+            ]
+        )
+
+        self.cycle_activity.start()
+
+    def cog_unload(self):
+        self.cycle_activity.cancel()
+
+    @tasks.loop(hours=1.0)
+    async def cycle_activity(self):
+        await self.bot.change_presence(activity=next(self.activities))
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild: Guild):
