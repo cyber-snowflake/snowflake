af1ca414b51dedfe08c16765b9541a392e8f4fcb
moret5u <72914371+MORET5U@users.noreply.github.com>
2021-02-09 22:16:30 +0400

    some stooped cmomandas aded

---
+++ tomodachi/cogs/default.py
@@ -0,0 +1,28 @@
+from tomodachi.core import Module
+
+from discord.ext import commands
+from discord import Member, Message
+import asyncio
+
+
+class Default(Module):
+    @commands.command(hidden=True)
+    async def hello(self, ctx: commands.Context):
+        await ctx.send(f"Hi, I'm {self.bot.user.name} and I'm a bot.")
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def clown(self, ctx: commands.Context, target: Member):
+        def check(m):
+            return m.author.id == target.id
+
+        try:
+            m: Message = await self.bot.wait_for("message", timeout=120.0, check=check)
+        except asyncio.TimeoutError:
+            pass
+        else:
+            await m.add_reaction("🤡")
+
+
+def setup(bot):
+    bot.add_cog(Default(bot))
