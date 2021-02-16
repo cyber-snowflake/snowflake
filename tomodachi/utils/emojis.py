b5379cdd6f1c059229e55c597c0814dde9ac946b
moret5u <72914371+MORET5U@users.noreply.github.com>
2021-02-16 15:17:38 +0400

    Updated Emojis utility class

--- tomodachi/utils/emojis.py
+++ tomodachi/utils/emojis.py
@@ -1,7 +1,7 @@
 import logging
 from collections import defaultdict
 from functools import cache
-from typing import Iterable, Union
+from typing import Callable, Iterable
 
 from discord import Emoji
 
@@ -10,21 +10,25 @@ from tomodachi.src.singleton import MetaSingleton
 __all__ = ["Emojis"]
 
 
+def default_emoji_factory():
+    return "\U0001f614"
+
+
 class Emojis(metaclass=MetaSingleton):
     __slots__ = ("_emojis",)
 
-    @staticmethod
-    def _default_emoji_factory():
-        return "\U0001f614"
-
     def __init__(self):
-        self._emojis: defaultdict[str, Union[Emoji, str]] = defaultdict(self._default_emoji_factory)
+        self._emojis = None
 
     @cache
     def __call__(self, name: str):
-        return str(self._emojis[name])
+        """Gets an emoji by name, if not found default emoji will be used"""
+        return self._emojis[name]
 
-    def setup(self, emojis: Iterable[Emoji]):
+    def setup(self, emojis: Iterable[Emoji], *, default_emoji: Callable = None):
+        factory = default_emoji or default_emoji_factory
+        self._emojis = defaultdict(factory)
+
         for emoji in set(emojis):
             self._emojis[emoji.name] = emoji
 
