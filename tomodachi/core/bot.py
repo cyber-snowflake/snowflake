d0ff64c4833e7de2c046d820595cc9cf0c7d6252
moretzu <72914371+moretzu@users.noreply.github.com>
2021-02-19 17:53:03 +0400

    added pg attr to bot, so it's easier to access

--- tomodachi/core/bot.py
+++ tomodachi/core/bot.py
@@ -10,6 +10,7 @@ import discord
 from discord.ext import commands
 
 import config
+from tomodachi.utils import pg
 
 __all__ = ["Tomodachi"]
 
@@ -23,6 +24,8 @@ async def get_prefix(bot, message: discord.Message):
 class Tomodachi(commands.AutoShardedBot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, command_prefix=get_prefix)
+
+        self.pg = pg()
 
         self.__once_ready_ = asyncio.Event()
         self.loop.create_task(self.once_ready())
