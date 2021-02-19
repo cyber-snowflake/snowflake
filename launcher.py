cd6999b42ecd5aacb4e23b5c15271980790e87c8
moretzu <72914371+moretzu@users.noreply.github.com>
2021-02-19 18:35:01 +0400

    runs the bot

--- launcher.py
+++ launcher.py
@@ -4,8 +4,23 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-from tomodachi.core.bot import Tomodachi
+import asyncio
+import importlib
+from typing import Any
 
+import config
+from tomodachi.core.bot import Tomodachi
+from tomodachi.utils import pg
+
+try:
+    uvloop: Any = importlib.import_module("uvloop")
+except ImportError:
+    pass
+else:
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(pg().setup(config.POSTGRES_DSN))
 
 tomodachi = Tomodachi()
 tomodachi.run()
