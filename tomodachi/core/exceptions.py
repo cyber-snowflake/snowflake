771861f2759569cd24e32459bf5dcd14fdd64862
moretzu <72914371+moretzu@users.noreply.github.com>
2021-02-20 19:22:11 +0400

    added an exception for a global rate limit

---
+++ tomodachi/core/exceptions.py
@@ -0,0 +1,18 @@
+#  Copyright (c) 2020 — present, moretzu (モーレツ)
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from typing import Union
+
+import discord
+from discord.ext.commands import CommandError
+
+__all__ = ["GloballyRateLimited"]
+
+
+class GloballyRateLimited(CommandError):
+    def __init__(self, user: Union[discord.User, discord.Member], retry_after: Union[int, float]):
+        self.user = user
+        self.retry_after = retry_after
