f7cf07c6b6aaab4eabc04a5ba4632c2a10a18439
moretzu <72914371+moretzu@users.noreply.github.com>
2021-02-21 23:32:22 +0400

    added user flags humanization

--- tomodachi/utils/text.py
+++ tomodachi/utils/text.py
@@ -4,6 +4,32 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+from collections import defaultdict
+
+__all__ = ["make_progress_bar", "HUMAN_READABLE_FLAGS"]
+
+
+_HUMAN_READABLE_FLAGS = {
+    "staff": "Discord Staff",
+    "partner": "Discord Partner",
+    "hypesquad": "HypeSquad Events",
+    "bug_hunter": "Discord Bug Hunter",
+    "bug_hunter_level_2": "Discord Bug Hunter",
+    "hypesquad_balance": "HypeSquad Balance",
+    "hypesquad_brilliance": "HypeSquad Brilliance",
+    "hypesquad_bravery": "HypeSquad Bravery",
+    "early_supporter": "Early Supporter",
+    "verified_bot": "Verified Bot",
+    "verified_bot_developer": "Verified Bot Developer",
+}
+
+
+def _human_readable_flags_factory():
+    return "Unknown flag"
+
+
+HUMAN_READABLE_FLAGS = defaultdict(_human_readable_flags_factory, _HUMAN_READABLE_FLAGS)
+
 
 def make_progress_bar(position: float, total: float, *, length: int = 15, filler="█", emptiness=" ", in_brackets=False):
     bar = ""
