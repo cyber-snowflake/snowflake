86066549ff20cbab02eb730ac62bdb835ca7af9a
moretzu <72914371+moretzu@users.noreply.github.com>
2021-02-21 21:43:48 +0400

    icons singleton container-class

---
+++ tomodachi/core/icons.py
@@ -0,0 +1,28 @@
+#  Copyright (c) 2020 — present, moretzu (モーレツ)
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from collections import defaultdict
+
+import discord
+
+from tomodachi.utils.singleton import MetaSingleton
+
+DEFAULT_EMOJI_FALLBACK = discord.PartialEmoji(name="U00002139")
+
+
+class Icons(metaclass=MetaSingleton):
+    __slots__ = ("_store",)
+
+    def __init__(self):
+        self._store = defaultdict(lambda: DEFAULT_EMOJI_FALLBACK)
+
+    def __call__(self, name: str):
+        return self._store[name]
+
+    async def setup(self, emojis: list[discord.Emoji]):
+        for e in emojis:
+            partial = discord.PartialEmoji(name=e.name, id=e.id)
+            self._store[e.name.lower()] = partial
