3c541b38be4a9448ed8d4fe948b21e022e1b1ce4
moretzu <72914371+moretzu@users.noreply.github.com>
2021-02-22 12:50:29 +0400

    holy shit PEP563 and typing module are quite amazing :O

--- tomodachi/core/checks.py
+++ tomodachi/core/checks.py
@@ -4,10 +4,17 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from tomodachi.core.exceptions import GloballyRateLimited
 
+if TYPE_CHECKING:
+    from tomodachi.core.bot import TomodachiContext
 
-async def spam_control(ctx):
+
+async def spam_control(ctx: TomodachiContext):
     bucket = ctx.bot.global_rate_limit.get_bucket(ctx.message)
     retry_after = bucket.update_rate_limit()
 
