d0a8e3881be2b90a91221fb85f35e0d07096d310
moretzu <72914371+moretzu@users.noreply.github.com>
2021-02-24 11:44:57 +0400

    what an amazing command! it can steal avatars!

---
+++ tomodachi/exts/owner.py
@@ -0,0 +1,51 @@
+#  Copyright (c) 2020 — present, moretzu (モーレツ)
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from __future__ import annotations
+
+import asyncio
+
+import discord
+from discord.ext import commands
+
+from tomodachi.core.bot import Tomodachi
+from tomodachi.core.context import TomodachiContext
+
+
+class Owner(commands.Cog):
+    def __init__(self, bot: Tomodachi):
+        self.bot = bot
+
+    async def cog_check(self, ctx: TomodachiContext):
+        return await self.bot.is_owner(ctx.author)
+
+    @commands.command()
+    async def steal_avatar(self, ctx: TomodachiContext, user: discord.User):
+        """Sets someone's avatar as bots' avatar"""
+        embed = discord.Embed(colour=0x2F3136)
+        embed.set_image(url=user.avatar_url)
+
+        await ctx.send(embed=embed, content="Are you sure that you want me to use this as avatar?\nSay `yes` or `no`.")
+
+        def check(m):
+            return m.author.id == ctx.author.id and str(m.content).lower() in ("yes", "no")
+
+        try:
+            message = await self.bot.wait_for("message", timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await ctx.send("Timed out.")
+        else:
+            if message.content.lower() == "yes":
+                b = await user.avatar_url.read()
+
+                await self.bot.user.edit(avatar=b)
+                await ctx.send(":ok_hand:")
+            else:
+                await ctx.send("Aborted.")
+
+
+def setup(bot):
+    bot.add_cog(Owner(bot))
